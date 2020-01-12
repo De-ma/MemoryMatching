a0a18e339c4e6a27a26082e277da41e081a738d4
De-ma <dabuadas@uoguelph.ca>
2020-01-12 17:15:42 -0500

    Shuffling and UI finalized

--- ShopifyW20/ViewController.swift
+++ ShopifyW20/ViewController.swift
@@ -1,47 +1,59 @@
-//
-//  ViewController.swift
-//  ShopifyW20
-//
-//  Created by Dema Abu Adas on 2019-09-22.
-//  Copyright © 2019 Dema Abu Adas. All rights reserved.
-//
-
 import UIKit
 import Moya
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var mainView: UIView!
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet weak var shuffleButton: UIButton!
+    @IBOutlet weak var matchedCardsLabel: UILabel!
+    
     lazy var game = MemoryMatching(numberOfPairsOfCards: cardButtons.count / 2)
     var matchedCards = 0 {
         didSet {
+            matchedCardsLabel.text = "Matched Cards: \(matchedCards)"
             if (matchedCards == cardButtons.count / 2) {
                 gameOver()
             }
         }
     }
     
+    var twoCardTouched = 0 {
+        didSet {
+            if (twoCardTouched == 1) {
+                shuffleButton.isEnabled = false
+            } else {
+                //no cards selected
+                shuffleButton.isEnabled = true
+            }
+        }
+    }
+
     
-    @IBOutlet var cardButtons: [UIButton]!
+    var emoji = [Int: String]()
+    var matchAttempts = 0
     static var collection: ShopifyProduct?
 
-    var emoji = [Int: String]()
-    var twoCardTouched = 0
-    var matchAttempts = 0
-
-    
     @IBAction func ShuffleCards(_ sender: UIButton) {
         game.shuffleCards()
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            button.backgroundColor = game.cards[index].isMatched ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 0) : #colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)
+            button.layer.borderWidth = game.cards[index].isMatched ? 0 : 2
+            button.setBackgroundImage(nil, for: .normal)
+        }
     }
     
     @IBAction func touchCard(_ sender: UIButton) {
         if let cardNumber = cardButtons.firstIndex(of: sender) {
             let matchedCard = game.chooseCard(at: cardNumber)
             updateViewFromModel()
-            
+
             if (matchedCard) {
                 matchedCards += 1
             }
+            print(twoCardTouched)
+
             
             twoCardTouched += 1
             if (twoCardTouched == 2) {
@@ -160,6 +172,7 @@ class ViewController: UIViewController {
     
     @objc func newGame() {
         game = MemoryMatching(numberOfPairsOfCards: cardButtons.count / 2)
+
         for index in cardButtons.indices {
             let button = cardButtons[index]
             button.isEnabled = true
@@ -167,6 +180,7 @@ class ViewController: UIViewController {
             button.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
             button.layer.borderWidth = 2
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5409764051, blue: 0.8473142982, alpha: 1)
+            button.setBackgroundImage(nil, for: .normal)
         }
         
         for subview in mainView.subviews {
@@ -177,5 +191,7 @@ class ViewController: UIViewController {
         
         matchAttempts = 0
         matchedCards = 0
+        twoCardTouched = 0
     }
+    
 }
